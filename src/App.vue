bde1609a701af23fce23df11f8c1d2028f50d6e4
BRKFun <burek@burek.com>
2021-11-05 22:00:08 +0300

    Harita eklendi

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Harita/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Harita from './components/Harita.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Harita,
   }
 }
 </script>
